4325e022ddfd6912948290226ed3db6223ccbf3b
Ameprizzo <amedeus926@gmail.com>
2020-02-13 12:47:59 +0300

    initial commit

---
+++ src/server/index.js
@@ -0,0 +1,24 @@
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+const app = express()
+
+app.use(express.static('src/client'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    res.sendFile('/client/views/index.html', {
+        root: __dirname + '/..'
+    })
+})
+const port = 5000;
+// designates what port the app will listen to for incoming requests
+app.listen(port, function () {
+    console.log(`Example app listening on localhost: ${port}`)
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})

13aa2eed387da0bf14218cb95ff6ade7b50de862
Ameprizzo <amedeus926@gmail.com>
2020-02-28 11:09:43 +0300

    commit

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,14 @@
-alert("i existed")
+import { checkForName } from './js/nameChecker'
+
+import './styles//resets.scss'
+import './styles/base.scss'
+import './styles/header.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+
+
+
+console.log(checkForName);
+
+alert("I EXIST")
+console.log("CHANGE!!");
